60abbda47ef7137cf27a88cb704aacb66abe250d
Frank Mitchell <me@frankmitchell.org>
2020-10-17 09:19:36 -0400

    Document the design token support in the JSON schema.

--- palettes/index.js
+++ palettes/index.js
@@ -1,5 +1,4 @@
 const aap64 = require('./aap-64.json');
-const zughy32 = require('./zughy-32.json');
 const solarized = require('./solarized.json');
 const okabeito = require('./okabe-ito.json');
 
@@ -7,5 +6,4 @@ module.exports = [
   solarized,
   okabeito,
   aap64,
-  zughy32,
 ];
