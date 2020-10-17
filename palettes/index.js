0ffb2cd447b4d13077ab8a2b846d6cd1652bc8aa
Frank Mitchell <me@frankmitchell.org>
2020-10-17 08:49:51 -0400

    Add the Color Universal Design pallete by Okabe & Ito.

--- palettes/index.js
+++ palettes/index.js
@@ -1,9 +1,11 @@
 const aap64 = require('./aap-64.json');
 const zughy32 = require('./zughy-32.json');
 const solarized = require('./solarized.json');
+const okabeito = require('./okabe-ito.json');
 
 module.exports = [
   solarized,
+  okabeito,
   aap64,
   zughy32,
 ];
