ee43fbaafae49ccd50802b4f7741ca1a9e089e4d
Frank Mitchell <me@frankmitchell.org>
2020-10-17 08:26:25 -0400

    Include Ethan Schoonover's Solarized palette.

--- palettes/index.js
+++ palettes/index.js
@@ -1,7 +1,9 @@
 const aap64 = require('./aap-64.json');
 const zughy32 = require('./zughy-32.json');
+const solarized = require('./solarized.json');
 
 module.exports = [
+  solarized,
   aap64,
   zughy32,
 ];
